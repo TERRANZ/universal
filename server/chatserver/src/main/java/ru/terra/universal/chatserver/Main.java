0a6280fe3ca025a877d0aaecb6ccf500a056fbd5
Вадим <vterranz@gmail.com>
2013-09-21 21:54:59 +0400

    now we can configure frontend ip address

--- server/chatserver/src/main/java/ru/terra/universal/chatserver/Main.java
+++ server/chatserver/src/main/java/ru/terra/universal/chatserver/Main.java
@@ -2,12 +2,16 @@ package ru.terra.universal.chatserver;
 
 import org.apache.log4j.BasicConfigurator;
 import ru.terra.universal.interserver.network.NetworkManager;
+import ru.terra.universal.shared.config.Config;
+import ru.terra.universal.shared.config.ConfigConstants;
 
 public class Main {
 
     public static void main(String[] args) {
         BasicConfigurator.configure();
-        NetworkManager.getInstance().start(ChatWorker.class, "127.0.0.1", 12346);
+        NetworkManager.getInstance().start(ChatWorker.class,
+                Config.getConfig().getValue(ConfigConstants.FRONTEND_IP, ConfigConstants.FRONTEND_IP_DEFAULT),
+                12346);
     }
 
 }
---
+++ server/shared/src/main/java/ru/terra/universal/shared/config/ConfigConstants.java
@@ -0,0 +1,6 @@
+package ru.terra.universal.shared.config;
+
+public interface ConfigConstants {
+    public static final String FRONTEND_IP = "frontend.ip";
+    public static final String FRONTEND_IP_DEFAULT = "127.0.0.1";
+}
