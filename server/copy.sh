12d329096d8fefeb0df101e1604bd96828d2122a
Vadim Korostelev <korostelev@smartsolutions-123.ru>
2015-11-23 13:13:46 +0400

    copy script

---
+++ server/copy.sh
@@ -0,0 +1,5 @@
+cp loginserver/target/loginserver-1-jar-with-dependencies.jar /virtual/universal/
+cp charserver/target/charserver-1-jar-with-dependencies.jar /virtual/universal/
+cp chatserver/target/chatserver-1-jar-with-dependencies.jar /virtual/universal/
+cp frontend/target/frontend-1-jar-with-dependencies.jar /virtual/universal/
+cp worldserver/target/worldserver-1-jar-with-dependencies.jar /virtual/universal/
